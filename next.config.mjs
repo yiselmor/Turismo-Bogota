86bbe192268e854e507bcaf85960f5ab3248447d
yiselmor <yiselunad2023@gmail.com>
2026-04-14 22:30:51 -0500

    Update next.config.mjs configuration
    Removed TypeScript build error ignoring and added basePath.

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
+  output: 'export',
   images: {
     unoptimized: true,
   },
-}
+  // Asegúrate de que esto coincida exactamente con el nombre de tu repositorio
+  basePath: '/turismo-bogota',
+};
 
-export default nextConfig
+export default nextConfig;
